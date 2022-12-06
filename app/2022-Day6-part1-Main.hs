cc10a768fadc7c9a5e62cb07315d2fb430e3ad2e
Ivan <ivan.costa@spendesk.com>
2022-12-06 10:47:10 +0100

    simplifications

--- app/2022-Day6-part1-Main.hs
+++ app/2022-Day6-part1-Main.hs
@@ -1,14 +1,15 @@
+
 module Main where
 import Data.List (nub)
 
 main :: IO ()
 main = do
   contents <- readFile "./2022-Day6.txt"
-  let marker = findMarker contents
+  let marker = findMarker 4 contents
   putStrLn ("result: " ++ show marker)
 
-findMarker :: (Eq a) => [a] -> Int
-findMarker =  marker . findWindowInEnumerate . enumerate
+findMarker :: (Eq a) => Int -> [a] -> Int
+findMarker i =  marker . findWindowInEnumerate i . enumerate
 
 marker :: [(Int, a)] -> Int
 marker = fst . last
@@ -16,16 +17,12 @@ marker = fst . last
 inspectWindow :: (Eq a) =>  [a] -> Bool
 inspectWindow w = (==) (length w) (length $ nub w)
 
-findWindowInEnumerate :: (Eq a) => [(Int, a)] -> [(Int, a)]
-findWindowInEnumerate (l1:l2:l3:[]) = []
-findWindowInEnumerate (l1:l2:l3:l4:rest) = 
-  let isPacket = inspectWindow $ map snd [l1,l2,l3,l4]
-  in
-    case (isPacket) of
-      True -> [l1,l2,l3,l4]
-      False -> findWindowInEnumerate $ [l2,l3,l4]++rest
+findWindowInEnumerate :: (Eq a) => Int -> [(Int, a)] -> [(Int, a)]
+findWindowInEnumerate i l
+  | inspectWindow $ map snd (take i l) = take i l
+  | otherwise = findWindowInEnumerate i (tail l) 
 
 enumerate :: [a] -> [(Int, a)]
 enumerate l = zip [1..length l] l
 
-test = 5 == findMarker "bvwbjplbgvbhsrlpgdmjqwftvncz"
+test = 5 == findMarker 4 "bvwbjplbgvbhsrlpgdmjqwftvncz"
--- app/2022-Day6-part2-Main.hs
+++ app/2022-Day6-part2-Main.hs
@@ -1,14 +1,15 @@
+
 module Main where
 import Data.List (nub)
 
 main :: IO ()
 main = do
   contents <- readFile "./2022-Day6.txt"
-  let marker = findMarker contents
+  let marker = findMarker 14 contents
   putStrLn ("result: " ++ show marker)
 
-findMarker :: (Eq a) => [a] -> Int
-findMarker =  marker . findWindowInEnumerate . enumerate
+findMarker :: (Eq a) => Int -> [a] -> Int
+findMarker i =  marker . findWindowInEnumerate i . enumerate
 
 marker :: [(Int, a)] -> Int
 marker = fst . last
@@ -16,17 +17,12 @@ marker = fst . last
 inspectWindow :: (Eq a) =>  [a] -> Bool
 inspectWindow w = (==) (length w) (length $ nub w)
 
-findWindowInEnumerate :: (Eq a) => [(Int, a)] -> [(Int, a)]
-findWindowInEnumerate l = 
-  let window = take 14 l
-      rest = drop 14 l
-      isPacket = inspectWindow $ map snd window 
-  in
-    case (isPacket) of
-      True -> window
-      False -> findWindowInEnumerate $ (tail window)++rest
+findWindowInEnumerate :: (Eq a) => Int -> [(Int, a)] -> [(Int, a)]
+findWindowInEnumerate i l
+  | inspectWindow $ map snd (take i l) = take i l
+  | otherwise = findWindowInEnumerate i (tail l) 
 
 enumerate :: [a] -> [(Int, a)]
 enumerate l = zip [1..length l] l
 
-test = 19 == findMarker "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+test = 19 == findMarker 14 "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
